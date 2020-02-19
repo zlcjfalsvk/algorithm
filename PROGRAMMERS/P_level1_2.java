d33fdba8655f836d201adf6fa0011943c9e539ab
zlcjfalsvk <zl0759vk@naver.com>
2020-02-20 00:17:30 +0900

    d

--- PROGRAMMERS/P_level1_2.java
+++ PROGRAMMERS/P_level1_2.java
@@ -1,4 +1,8 @@
 package PROGRAMMERS;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * P_level1_2 완주하지 못한 선수
  */
@@ -9,24 +13,45 @@ class P_level1_2 {
         String[] a = { "kiki", "leo", "eden", "leo", "kiki" };
         String[] a2 = { "leo", "kiki", "eden", "kiki" };
         System.out.println(l.solution(a, a2));
-    }
-
-    public String solution(String[] participant, String[] completion) {
-        for (int i = 0; i < completion.length; i++) {
-            for (int j = 0; j < participant.length; j++) {
-                if(completion[i] == null) break;
-                if(participant[j] != null && participant[j].equals(completion[i])){
-                    participant[j] = null;
-                    completion[i] = null;
-                    break;
-                }
-            }
         }
 
-        for (int j = 0; j < participant.length; j++) {
-            if(participant[j]!= null) {
-                return participant[j];
-            } 
+    // 위와 같이 하면 효율성에서 좋은 점수를 받지 못함
+    // public String solution(String[] participant, String[] completion) {
+    //     for (int i = 0; i < completion.length; i++) {
+    //         for (int j = 0; j < participant.length; j++) {
+    //             if(completion[i] == null) break;
+    //             if(participant[j] != null && participant[j].equals(completion[i])){
+    //                 participant[j] = null;
+    //                 completion[i] = null;
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     for (int j = 0; j < participant.length; j++) {
+    //         if(participant[j]!= null) {
+    //             return participant[j];
+    //         } 
+    //     }
+    //     return "";
+    // }
+
+    // 해시를 이용한 방법
+    public String solution(String[] participant, String[] completion) {
+        
+        HashMap<String, Integer> hash = new HashMap<>();
+        
+        for (String string : participant) {
+            hash.put(string, hash.getOrDefault(string, 0) + 1);            
+        }
+
+        for (String string : completion) {
+            hash.put(string, hash.get(string) - 1);            
+        }
+
+        for(Map.Entry<String, Integer> entry: hash.entrySet()) {
+            if(entry.getValue() != 0) {
+                return entry.getKey();
+            }
         }
         return "";
     }
---
+++ PROGRAMMERS/P_level1_3.java
@@ -0,0 +1,35 @@
+package PROGRAMMERS;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * K번째 수
+ */
+public class P_level1_3 {
+
+
+    public static void main(final String[] args) {
+        int[] answer = { 1, 5, 2, 6, 3, 7, 4 };
+        int[][] commands = { { 2, 5, 3 }, { 4, 4, 1 }, { 1, 7, 3 } };
+        int[] a = solution(answer, commands);
+        System.out.println(a);
+    }
+
+    public static int[] solution(final int[] array, final int[][] commands) {
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < commands.length; i++) {
+            final int startIndex = commands[i][0] - 1;
+            final int endIndex = commands[i][1];
+            final int findIndex = commands[i][2] - 1;
+            answer.add(solution(Arrays.copyOfRange(array, startIndex, endIndex), startIndex, endIndex, findIndex));
+        }
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public static int solution(int[] arr, final int startI, final int endI, final int findI) {
+        Arrays.sort(arr);
+        return arr[findI] ;
+    }
+}

37b72bd3adcca83055239f6f5ed5199108939b28
zlcjfalsvk <zl0759vk@naver.com>
2020-02-25 22:00:32 +0900

    Create P_level1_4.java

---
+++ PROGRAMMERS/P_level1_4.java
@@ -0,0 +1,19 @@
+package PROGRAMMERS;
+
+/**
+ * 약수의 합
+ * https://programmers.co.kr/learn/courses/30/lessons/12928
+ */
+public class P_level1_4 {
+
+    class Solution {
+        public int solution(int n) {
+            if(n <= 1) return n;
+            int answer = 1 + n;
+            for(int i = 2 ; i <= n/2 ; i ++) {
+                if(n % i == 0) answer+=i;
+            }
+            return answer;
+        }
+      }
+}
